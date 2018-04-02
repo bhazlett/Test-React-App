a6a1add55db3d3dc43ab4425319914b24f53cd46
bhazlet <hben1@umbc.edu>
2018-04-02 16:45:14 -0400

    third commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,14 +5,20 @@ class Square extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value: null,
+			squares: Array(9).fill(null),
 		};
 	}
-	
+
+	renderSquare(i) {
+		return <Square value={i} />;
+	}
+
     render() {
+    
 		return (
-	  		<button className="square" onClick={() => alert('click')}>
-				{this.props.value}
+	  		<button className="square" onClick={() => 
+	  			this.setState({value: 'X'})}>
+				{this.state.value}
 	    	</button>
 		);
 	}
