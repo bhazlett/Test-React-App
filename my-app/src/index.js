1bb418f8c5f48101016e50e8c7839ef0da7e3bf5
bhazlet <hben1@umbc.edu>
2018-04-02 16:23:17 -0400

    First commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
     render() {
-	return (
-	        <button className="square">
-		{/* TODO */}
-	    </button>
-	);
-    }
+		return (
+	  		<button className="square">
+				{this.props.value}
+	    	</button>
+		);
+	}
 }
 
 class Board extends React.Component {
     renderSquare(i) {
-	return <Square />;
+	return <Square value={i} />;
     }
 
     render() {
